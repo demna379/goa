c47e573fb234dd0f9bd889d8504c31530219d76a
demna379 <tao.georgia@gmail.com>
2025-02-21 14:52:22 +0400

    second commit

--- day 03/homework/lesson03.py
+++ day 03/homework/lesson03.py
@@ -1,1 +1,48 @@
-# შექმენიტ 9 ცვლადი,
+# 1) შექმენიტ 9 ცვლადი, 3 ცვლადი იყოს str, 3 ცვლადი int, 3 ცვლადი float, და შემდეგ გამოიტანე ტერმინალზე 9-ვე ცვლადი cvladi print-ის მეშვეობით
+# str
+country="japan"
+city="sapporo"
+capital="tokyo"
+#int
+number=12
+february=17
+money=100
+#float
+time=6.01
+coins=12.01
+money_pocket=13.1
+# str
+print(country)
+print(city)
+print(capital)
+# int
+print(number)
+print(february)
+print(money)
+# float
+print(time)
+print(coins)
+print(money_pocket)
+
+# 2) კომენტარის სახით დაწერე დღეს ნასწავლი Data type's ანუ მონაცემთა ტიპები და ახსენით თითეული მონაცემთა ტიპი რისთვის გამოიყენება 
+
+#str არის ნებოისმიერი სიტყვა ან რიცხვი ბრჩყალებში ჩასმული, მაგალითად "გამარჯობა" "20" "привет"
+#int არის ნემისმიერი რიცხვი, მაგალითად 2; 3; 6.
+#float არის ათწილადი/წერტილიანი რიცხვი, მაგალითად 10.2; 11.9
+
+# 3) შექმენი 8 ცვლადი, და გამოიყენე Math operators --> (+, -, /, * ) ყველა ცვლადზე
+print(10+10)
+print(2-1)
+print(20*1000)
+print(20/2)
+print(10+1193)
+print(34/4)
+print(10938293839282-938392829)
+print(9+1)
+
+# 4) შექმენი 4 Str ცვლადი და დაწერე 1 დიდი წინადადება
+country="russia"
+city="novosibirk"
+capital="moswcow"
+population="144Million"
+print(country+" "+city+" "+capital+" "+population)
---
+++ day 4/homework/lesson04.py
@@ -0,0 +1,46 @@
+# 1)შექმენით 3 ცვლადი და თითო განაახლე 5 ჯერ შემდეგ შედეგი გამოიტანე print-ით
+country="russia"
+country="georgia"
+country="china"
+country="america"
+country="india"
+
+number=5
+number=4
+number=56
+number=2
+number=45
+
+temperature="46f"
+temperature="123f"
+temperature="500f"
+temperature="50f"
+temperature="-200f"
+
+print(country)
+print(number)
+print(temperature)
+
+# 2)გაასწორე შეცოდმები და კომენტარის სახით მიუწერე რა არის შეცდომა:
+# 1
+num1 = "one" #აქ შეცდომა ის არის რომ "" მაგივრად გამოვიყენეთ ;; 
+print(num1)
+# 2
+num2 = ('two') # აქ ცვლადი არ გვაქვს
+# 3
+Num3 = 'three'
+print("num3") # აქ ცვლადი უნდა ჩავსვათ ბრჭყაკებში 
+# 4
+num4 = "number4" 
+print(num4) # ორი ბრჭყალი უნდა 
+# 5
+num5 = 'five'
+print(num5) # ციფრის უკან მიწერა არ შეიძლება 
+
+# 4)შექმენი 4 int ცვლადი და შეინახე ნებისმიერი int რიცხვები, შექმენი მე-5 ცვლადში შეინახე ამ 10 ცვლადის დამატებული, და შემდეგ print ის საშვალებით დაპრინტე მე 5 ცვალდი.
+number=4
+number1=9
+number2=1
+number3=10
+number4=number+number1+number2+number3
+print(number4)
