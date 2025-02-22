392f7bb2c82f0b0ad0e1bd67aef1e9277a811167
demna379 <tao.georgia@gmail.com>
2025-02-22 22:00:51 +0400

    second commit

--- day 4/classwork/lesson4.py
+++
@@ -1,5 +0,0 @@
-print(hello #brchyalebi da brchxilebi unda mivuwerot ase print("hello")
-name = deme #ama brchyalebi ar aqvs da ase unda dagvewera name="deme"
-surname - "maisuradze" #aq equal sign unda davwerot ase surname=maisuradze
-print("maisuradze" #aq brchxilebshi unda chavsvat ase print("maisuradze")
-print(hi) #aq brcyalebshi unda chavsvat
